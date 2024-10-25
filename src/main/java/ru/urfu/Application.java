f47877626725bb1cb3e894aa6279f5ad989d17d2
Alexandr Filatov <filatovalexandr2003@gmail.com>
2024-10-25 12:00:00 +0500

    Add task oop architecture

---
+++ src/main/java/ru/urfu/Application.java
@@ -0,0 +1,21 @@
+package ru.urfu;
+
+/**
+ * Класс для запуска приложения
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        String telegramBotName = System.getenv("telegram_botName");
+        String telegramToken = System.getenv("telegram_token");
+        new TelegramBot(telegramBotName, telegramToken)
+                .start();
+
+        String discordToken = System.getenv("discord_token");
+        new DiscordBot(discordToken)
+                .start();
+
+        // сколько угодно чат платформ и все должны работать одинаково
+    }
+
+}
