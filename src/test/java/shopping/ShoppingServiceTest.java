61f72e2f2b1e6b3b993429a66ea4de6594e19f74
Alexandr Filatov <filatovalexandr2003@gmail.com>
2024-11-19 19:08:35 +0500

    fix: buy test

--- src/test/java/shopping/ShoppingServiceTest.java
+++ src/test/java/shopping/ShoppingServiceTest.java
@@ -4,9 +4,7 @@ import customer.Customer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import product.Product;
 import product.ProductDao;
 
@@ -15,19 +13,21 @@ import java.util.List;
 /**
  * Тесты сервиса ShoppingService
  */
-@ExtendWith(MockitoExtension.class)
 public class ShoppingServiceTest {
 
-    private ProductDao productDao = Mockito.mock(ProductDao.class);
+    private final ProductDao productDao = Mockito.mock(ProductDao.class);
 
-    private ShoppingService shoppingService
-            = new ShoppingServiceImpl(productDao);
+    private final ShoppingService shoppingService = new ShoppingServiceImpl(productDao);
 
-    Product product;
+    private Product product;
+    private Customer customer;
+    private Cart cart;
 
     @BeforeEach
     public void setUp() {
+        customer = new Customer(1, "+780005553535");
         product = new Product("milk", 2);
+        cart = new Cart(customer);
     }
 
     /**
@@ -35,14 +35,9 @@ public class ShoppingServiceTest {
      */
     @Test
     public void getCartTest(){
-        Customer customer = new Customer(1, "+780005553535");
         Cart cart = shoppingService.getCart(customer);
 
         Assertions.assertTrue(cart.getProducts().keySet().isEmpty());
-
-        cart.add(product, 1);
-
-        Assertions.assertTrue(cart.getProducts().containsKey(product));
     }
 
     /**
@@ -72,8 +67,6 @@ public class ShoppingServiceTest {
      */
     @Test
     public void buyProductTest_Success() throws BuyException {
-        Customer customer = new Customer(1, "+780005553535");
-        Cart cart = shoppingService.getCart(customer);
         cart.add(product, 1);
 
         Assertions.assertTrue(shoppingService.buy(cart));
@@ -82,25 +75,27 @@ public class ShoppingServiceTest {
 
     /**
      * Тест покупки продуктов, которых нет в корзине - покупка не состоится
-     * @throws BuyException
      */
     @Test
     public void buyProductTest_EmptyCart() throws BuyException {
-        Customer customer = new Customer(1, "+780005553535");
-        Cart cart = shoppingService.getCart(customer);
-
+        Assertions.assertTrue(cart.getProducts().isEmpty());
+        Assertions.assertEquals(2, product.getCount());
         Assertions.assertFalse(shoppingService.buy(cart));
     }
 
     /**
-     * Не надо тестировать, т.к. невозможно добавить товар в корзину, если его нет в наличии.
-     * Более того тут и содержится логическая ошибка, кол-во товара при добавлении в корзину и при покупке
-     * не уменьшается
+     * Тест выбрасывания исключения, если товар закончился
      */
     @Test
-    public void buyProductTest_BuyException() throws BuyException {
+    public void buyProductTest_BuyException() throws BuyException {cart.add(product, 1);
+        cart.add(product, 1);
+
+        Assertions.assertTrue(shoppingService.buy(cart));
+        Assertions.assertTrue(shoppingService.buy(cart));
+        Assertions.assertThrows(BuyException.class, () -> shoppingService.buy(cart),
+                "В наличии нет необходимого количества товара " + product.getName());
+
+        Mockito.verify(productDao, Mockito.times(2)).save(product);
     }
-
-
 }
 
